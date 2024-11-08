7043886effb0cb94cf181d6864fe02a711bad73d
Jay Robson <jsrobson10@gmail.com>
2024-11-09 00:58:54 +1100

    add examples

---
+++ examples/fibonacci.asm
@@ -0,0 +1,20 @@
+
+counter = .
+display_ptr = .
+a = .
+b = .
+c = .
+
+	copy_v 0, a
+	copy_v 1, b
+	copy_v 16, counter
+scan:
+	math_pp.add a, b, c
+	copy_p b, a
+	copy_p c, b
+	math_pv.sub counter, 1, counter
+	math_pv.add counter, 0xf0, display_ptr
+	deref_s a, display_ptr
+	jump_nz counter, scan
+
+
